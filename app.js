78fe7ec751751473d849376e42404ddf973db895
haoxle <hao-le@live.co.uk>
2022-10-06 10:48:14 +0100

    styling and checklist

---
+++ app.js
@@ -0,0 +1,18 @@
+/*Game project
+
+// Project Setup
+// Generate Map
+// Add Maplestory Character with movement
+// Add collision detection
+// Swap boundaries with images
+// Create enemy
+// Create power-ip
+// Add win condution
+// Mobile and computer conversion
+
+
+1. Create a canvas/div, query selector and console log to see what you have done.
+2. Creating a canvas context const c = canvas.getContext('2d') //read more about this
+3. canvas.width = window.innderWidth (probaby do this on css)
+4. canvas.height= innerheight
+5. set margin to 0 */

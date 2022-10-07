90678f1e2e6b5b85259c63b95bb9a73c79ef6b82
Hao Le <hao-le@live.co.uk>
2022-10-07 17:19:20 +0100

    working map2 transfer start btn rmv

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ imgSpear.src = "./Images/spear.png";
 let charY = 80;
 let charX = 0;
 let go = 0.5;
-// let chosenMap = mapOne;
 
 const keyClick = (e) => {
   if (e.key == "Right" || e.key == "ArrowRight") {
@@ -77,90 +76,92 @@ const moving = () => {
 document.addEventListener("keydown", keyClick, false);
 document.addEventListener("keyup", KeyRelease, false);
 
-const map1 = () => {
-  moving();
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.beginPath();
-  context.drawImage(girl, charX, charY, charWidth, charHeight);
-  context.fillStyle = "#0095DD";
-  context.fill();
-  context.closePath();
-  context.beginPath();
-  context.drawImage(img1, 0, 0, img1w, img1h);
-  context.drawImage(img1, 0, 140, img1w, img1h);
-  context.drawImage(img1, 0, 210, img1w, img1h);
-  context.drawImage(img1, 70, 0, img1w, img1h);
-  context.drawImage(img1, 70, 210, img1w, img1h);
-  context.drawImage(img1, 70, 210, img1w, img1h);
-  context.drawImage(img1, 140, 0, img1w, img1h);
-  context.drawImage(img1, 140, 70, img1w, img1h);
-  context.drawImage(img1, 140, 210, img1w, img1h);
-  context.drawImage(img1, 210, 70, img1w, img1h);
-  context.drawImage(img1, 210, 210, img1w, img1h);
-  context.drawImage(img1, 280, 70, img1w, img1h);
-  context.drawImage(img1, 280, 210, img1w, img1h);
-  context.drawImage(img1, 350, 210, img1w, img1h);
-  context.drawImage(img1, 420, 210, img1w, img1h);
-  context.drawImage(img1, 420, 280, img1w, img1h);
-  context.drawImage(img1, 420, 0, 140, 140);
-  context.drawImage(imgSpear, 500, 300, 50, 50);
-  context.closePath();
-  if (
-    charX + charWidth >= 480 &&
-    charX <= 560 &&
-    charY + charHeight >= 300 &&
-    charY <= 350
-  ) {
-    map2();
-  }
-  if (
-    (charX + charWidth >= 0 &&
-      charX <= 210 &&
-      charY + charHeight >= 0 &&
-      charY <= 60) +
-    (charX + charWidth >= 180 &&
-      charX <= 320 &&
-      charY + charHeight >= 80 &&
-      charY <= 125) +
-    (charX + charWidth >= 0 &&
-      charX <= 60 &&
-      charY + charHeight >= 150 &&
-      charY <= 210) +
-    (charX + charWidth >= 0 &&
-      charX <= 480 &&
-      charY + charHeight >= 230 &&
-      charY <= 280) +
-    (charX + charWidth >= 420 &&
-      charX <= 465 &&
-      charY + charHeight >= 280 &&
-      charY <= 350) +
-    (charX + charWidth >= 470 &&
-      charX <= 560 &&
-      charY + charHeight >= 0 &&
-      charY <= 125)
-  ) {
-    return (go = go * -1);
-  } else {
-    return (go = 0.5);
-  }
-};
-
-const map2 = () => {
-  moving();
-  context.clearRect(0, 0, canvas.width, canvas.height);
-  context.beginPath();
-  context.drawImage(girl, charX, charY, charWidth, charHeight);
-  context.fillStyle = "#0095DD";
-  context.fill();
-  context.closePath();
-  context.beginPath();
-  context.drawImage(img1, 0, 0, img1w, img1h);
-  context.drawImage(img1, 0, 140, img1w, img1h);
-  context.drawImage(imgSpear, 240, 0, 50, 50);
-  context.closePath();
-};
-
 const letsPlay = (e) => {
-  setInterval(map1);
+  play.classList.add("hide");
+  const map1 = () => {
+    moving();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.drawImage(girl, charX, charY, charWidth, charHeight);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
+    context.beginPath();
+    context.drawImage(img1, 0, 0, img1w, img1h);
+    context.drawImage(img1, 0, 140, img1w, img1h);
+    context.drawImage(img1, 0, 210, img1w, img1h);
+    context.drawImage(img1, 70, 0, img1w, img1h);
+    context.drawImage(img1, 70, 210, img1w, img1h);
+    context.drawImage(img1, 70, 210, img1w, img1h);
+    context.drawImage(img1, 140, 0, img1w, img1h);
+    context.drawImage(img1, 140, 70, img1w, img1h);
+    context.drawImage(img1, 140, 210, img1w, img1h);
+    context.drawImage(img1, 210, 70, img1w, img1h);
+    context.drawImage(img1, 210, 210, img1w, img1h);
+    context.drawImage(img1, 280, 70, img1w, img1h);
+    context.drawImage(img1, 280, 210, img1w, img1h);
+    context.drawImage(img1, 350, 210, img1w, img1h);
+    context.drawImage(img1, 420, 210, img1w, img1h);
+    context.drawImage(img1, 420, 280, img1w, img1h);
+    context.drawImage(img1, 420, 0, 140, 140);
+    context.drawImage(imgSpear, 500, 300, 50, 50);
+    context.closePath();
+    if (
+      charX + charWidth >= 480 &&
+      charX <= 560 &&
+      charY + charHeight >= 300 &&
+      charY <= 350
+    ) {
+      clearInterval(loadMap);
+      loadMap = setInterval(map2);
+    }
+    if (
+      (charX + charWidth >= 0 &&
+        charX <= 210 &&
+        charY + charHeight >= 0 &&
+        charY <= 60) +
+      (charX + charWidth >= 180 &&
+        charX <= 320 &&
+        charY + charHeight >= 80 &&
+        charY <= 125) +
+      (charX + charWidth >= 0 &&
+        charX <= 60 &&
+        charY + charHeight >= 150 &&
+        charY <= 210) +
+      (charX + charWidth >= 0 &&
+        charX <= 480 &&
+        charY + charHeight >= 230 &&
+        charY <= 280) +
+      (charX + charWidth >= 420 &&
+        charX <= 465 &&
+        charY + charHeight >= 280 &&
+        charY <= 350) +
+      (charX + charWidth >= 470 &&
+        charX <= 560 &&
+        charY + charHeight >= 0 &&
+        charY <= 125)
+    ) {
+      return (go = go * -1);
+    } else {
+      return (go = 0.5);
+    }
+  };
+  const map2 = () => {
+    moving();
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.beginPath();
+    context.drawImage(girl, charX, charY, charWidth, charHeight);
+    context.fillStyle = "#0095DD";
+    context.fill();
+    context.closePath();
+    context.beginPath();
+    context.drawImage(img1, 0, 0, img1w, img1h);
+    context.drawImage(img1, 0, 140, img1w, img1h);
+    context.drawImage(imgSpear, 240, 0, 50, 50);
+    context.closePath();
+  };
+
+  let loadMap = setInterval(map1);
 };
-document.addEventListener("click", letsPlay);
+
+play.addEventListener("click", letsPlay);
